2019dacb8179c5605aedc2084c6bcb35692cc7a8
Antonio-YKD <antonio67.ykd@gmail.com>
2025-01-09 21:47:55 -0600

    I finished number 4

---
+++ src/isVowel/funcion.java
@@ -0,0 +1,11 @@
+package isVowel;
+
+public class funcion {
+
+	public static boolean isVowel(char Vowel) {
+		if (Vowel == ('a') || Vowel == ('e') || Vowel == ('i') || Vowel == ('o') || Vowel == ('u')) {
+			return true;
+		}
+		return false;
+	}
+}
