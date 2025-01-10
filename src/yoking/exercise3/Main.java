ef20654cecda234b315a8fc0a8fc259d8c5c1302
Antonio-YKD <antonio67.ykd@gmail.com>
2025-01-09 19:51:05 -0600

    finish the third exercise

--- src/yoking/exercise3/Main.java
+++ src/yoking/exercise3/Main.java
@@ -1,20 +1,41 @@
 package yoking.exercise3;
 
+import java.util.Scanner;
+
 public class Main {
-	//Desarrollar un programa que tome del usuario una palabra y un numero de salto
-	//Segun esto, colocar una x cada vez que llegue dicho salto entre la palabra
-	
-	/*
-	 * Ejemplos:
-	 * 
-	 * ejemplo1: palabra: "perro" salto: 2
-	 * resultado: pexrrxo
-	 * 
-	 * ejemplo2: palabra: "palabra" salto: 3
-	 * resultado: palxabrxa
-	 * 
-	 * ejemplo3: palabra: "sol" salto: 4
-	 * resultado: sol
-	 * (Nota como la palabra es muy corta como para poner un salto)
-	 */
+	public static void main(String[] args) {
+
+		// Desarrollar un programa que tome del usuario una palabra y un numero de salto
+		// Segun esto, colocar una x cada vez que llegue dicho salto entre la palabra
+
+		/*
+		 * Ejemplos:
+		 * 
+		 * ejemplo1: palabra: "perro" salto: 2 resultado: pexrrxo
+		 * 
+		 * ejemplo2: palabra: "palabra" salto: 3 resultado: palxabrxa
+		 * 
+		 * ejemplo3: palabra: "sol" salto: 4 resultado: sol (Nota como la palabra es muy
+		 * corta como para poner un salto)
+		 */
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Ingresa tu palabra");
+		String action = scan.nextLine();
+		int counter = 0;
+
+		System.out.println("Ingresa el numero de saltos que deseas poner: ");
+		int jumps = scan.nextInt();
+
+		for (char Letter : action.toCharArray()) {
+			System.out.println(Letter);
+			counter++;
+			if (counter == jumps) {
+				System.out.println("x");
+				counter -= jumps;
+			}
+
+		}
+
+	}
 }
