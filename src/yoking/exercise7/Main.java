10bfe4c2ce2ccc08a4d596a5cc9e984d8bf6f5fe
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2025-01-17 09:19:33 -0600

    4 block of exercises

--- src/yoking/exercise7/Main.java
+++ src/yoking/exercise7/Main.java
@@ -1,5 +1,20 @@
 package yoking.exercise7;
 
 public class Main {
-	//Due to Thursday 16
+	/*
+	 * Desarrollar un programa que valide una contraseña
+	 * Se solicitará al usuario que cree una contraseña
+	 * Luego se debe evaluar en la string resultante:
+	 * 1. que la contraseña tenga una longitud por lo menos de 6 caracteres 
+	 * (pueden ser mas)
+	 * 2. debe contener por lo menos 3 letras (pueden ser mas)
+	 * 3. debe contener por lo menos 3 numeros (pueden ser mas)
+	 * 4. debe contar con el caracter * 
+	 * 
+	 * Finalmente, si cumple todos los requisitos, dirá "Contraseña correcta"
+	 * y deberá finalizar el programa
+	 * En caso de que no cumpla con los requisitos, deberá indicar "Contraseña
+	 * muy débil" y deberá PERMITIRLE ingresar la contraseña nuevamente
+	 * hasta que logre colocar una lo suficientemente fuerte.
+	 */
 }
--- src/yoking/exercise8/Main.java
+++ src/yoking/exercise8/Main.java
@@ -1,5 +1,17 @@
 package yoking.exercise8;
 
 public class Main {
-	//Due to Thursday 16
+	/*
+	 * Desarrollar un programa que permita registrar un usuario
+	 * 
+	 * Al iniciar el programa, deberá intentar leer un archivo con el usuario,
+	 * 
+	 * En caso de que no lo encuentre:
+	 * Se le indicará al usuario que coloque su nombre, su edad y su correo electrónico
+	 * Luego, se ejecutará una función (debe ser creada aparte) llamada "registerUser"
+	 * la cual escribirá en un archivo las características que ingresó el usuario
+	 * 
+	 * En caso de que sí lo encuentre, se le dará la bienvenida al usuario indicando
+	 * los datos que había metido anteriormente.
+	 */
 }
